fff3270016dbd16af874f0b643ee0f3212117a43
michelledayana <michurivadeneira98@gmail.com>
2025-07-14 01:02:14 -0500

    feat: :sparkles: Create User entity for the Users table with basic fields

---
+++ Backend/userDomain/user_update_service/src/main/java/com/usuario/model/User.java
@@ -0,0 +1,20 @@
+package com.usuario.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    // getters y setters
+}

997de9317c8c2adaf0d8084d89c67aff703f081e
michelledayana <michurivadeneira98@gmail.com>
2025-07-15 23:53:38 -0500

    feat: :sparkles: Create a User model with Sequelize for the Users table

---
+++ Backend/userDomain/user_delete_service/models/user.js
@@ -0,0 +1,18 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/db.js';
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING
+}, {
+  tableName: 'users',
+  timestamps: false
+});
+
+export default User;

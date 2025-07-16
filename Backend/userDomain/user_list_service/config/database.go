98805584349ea7cdbe90412ac50f1f8e0143652d
michelledayana <michurivadeneira98@gmail.com>
2025-07-16 02:02:41 -0500

    feat: :wrench: configure connection to MySQL database with GORM

---
+++ Backend/userDomain/user_list_service/config/database.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error conectando a la base de datos: %v", err)
+	}
+
+	DB = database
+	fmt.Println("Conexión a MySQL exitosa")
+}

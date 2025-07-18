29819bbdf3d3bcc5c9a27550e84dd5b46164d4c7
michelledayana <michurivadeneira98@gmail.com>
2025-07-18 12:46:36 -0500

    fix: :bug: corrected connection.py and Dockerfile for proper service functionality

--- Backend/productDomain/product_list_service/app/database/connection.py
+++ Backend/productDomain/product_list_service/app/database/connection.py
@@ -1,7 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:dayana@localhost:3306/products_db"
+load_dotenv()  # carga el archivo .env
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

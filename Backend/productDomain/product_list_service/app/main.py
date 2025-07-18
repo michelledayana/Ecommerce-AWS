97072fbd68b6c7f8e4a6666ec90e7306cf46d3e6
michelledayana <michurivadeneira98@gmail.com>
2025-07-18 11:55:33 -0500

    feat: ✨ initialize FastAPI app and auto-generate product table

---
+++ Backend/productDomain/product_list_service/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.routes import product_routes
+from app.database.connection import engine
+from app.models.product_model import Product
+
+app = FastAPI()
+
+# Crea las tablas automáticamente al iniciar
+Product.metadata.create_all(bind=engine)
+
+# Registra las rutas
+app.include_router(product_routes.router)

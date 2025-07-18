a022300caa94d26b0d76cb1a51f2e3a6a1a53f41
michelledayana <michurivadeneira98@gmail.com>
2025-07-18 10:35:22 -0500

    feat: :rocket: initialize server and register product update route

---
+++ Backend/productDomain/product_update_service/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"product-update-service/controller"
+	"product-update-service/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	database.Connect()
+
+	router := gin.Default()
+
+	// Ruta para actualizar producto
+	router.PUT("/products/:id", controller.UpdateProductHandler)
+
+	router.Run(":3015")
+}

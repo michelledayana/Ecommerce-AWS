a34e99ed8a0ec3571a27f76063aa09589605f62e
michelledayana <michurivadeneira98@gmail.com>
2025-07-18 11:30:18 -0500

    chore: 📁 initialize folder structure for product_list_service microservice

--- Backend/productDomain/product_update_service/database/connection.go
+++ Backend/productDomain/product_update_service/database/connection.go
@@ -1,9 +1,9 @@
-// database/connection.go
 package database
 
 import (
 	"fmt"
 	"log"
+	"os"
 	"product-update-service/model"
 
 	"gorm.io/driver/postgres"
@@ -13,7 +13,13 @@ import (
 var DB *gorm.DB
 
 func Connect() *gorm.DB {
-	dsn := "host=localhost user=postgres password=dayana dbname=products_db port=5432 sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error al conectar a la base de datos:", err)
